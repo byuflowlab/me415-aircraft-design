069305f1322d272132df92d271fcc93f822bc047
rymanderson <rymanderson@gmail.com>
2022-09-29 10:19:02 -0600

    added motor sizing, vertical climb and cruise energy estimates, and battery weight and volume estimators; still need to predict descent energy

--- motor_func.jl
+++ motor_func.jl
@@ -1,13 +1,20 @@
-#=
-These models are taken from NDARC NASA Design and Analysis of Rotorcraft Theory
-@techreport{johnson2017ndarc,
-  title={NDARC NASA design and analysis of rotorcraft},
-  author={Johnson, Wayne},
-  year={2017}
-}
-=#
+"Accepts power in W and returns motor mass in kg.
 
-# specify peak-torque in foot-pounds
-motor_weight_nasa15_high_torque(peak_torque) = 0.3928 * peak_torque^0.8587
+Taken from  Duffy, M., Sevier, A., Hupp, R., Perdomo, E., Wakayama, S., “Propulsion Scaling Methods in the Era of Electric Flight,” AIAA Propulsion and Energy Forum, AIAA/IEEE Electric Aircraft Technologies Symposium, 9 July, 2018. doi: 10.2514/6.2018- 4978."
+motor_mass_duffy(power) = 0.2322 * power / 1000
 
-motor_weight_nasa15(peak_torque, f=2.5606) = 0.5382 * f * peak_torque^0.8129
+"Momentum theory, assuming figure of merit of 0.7 and hover conditions."
+rotor_power(thrust, area, air_density, v_climb, fom=0.7) = (1 + v_climb/2) * thrust * sqrt(thrust/2/air_density/area) / fom
+
+function motor_mass(max_thrust, area, air_density, v_climb)
+    power = rotor_power(max_thrust, area, air_density, v_climb)
+    mass = motor_mass_duffy(power)
+end
+
+"V_climb default specified in “UberAir Vehicle Requirements and Missions,” Uber, Uber Elevate, 2019. [retrieved 7 November 2019]."
+function motor_mass_vehicle(vehicle_weight, n_rotors, rotor_diameter; v_climb = 500 * 0.3048 / 60, air_density = 1.225)
+    thrust_per_rotor = vehicle_weight / n_rotors
+    area = rotor_diameter^2/4*pi
+    single_motor_mass = motor_mass(thrust_per_rotor, area, air_density, v_climb)
+    return single_motor_mass
+end
